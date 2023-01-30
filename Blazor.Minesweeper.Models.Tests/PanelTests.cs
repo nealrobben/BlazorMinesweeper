7a58df71b8b1400a06c41210ffb7e71b02df0450
DESKTOP-8B4QBQ3\Windows <neal.robben@gmail.com>
2023-01-30 21:26:16 +0100

    Added unit tests for Panel

---
+++ Blazor.Minesweeper.Models.Tests/PanelTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+
+namespace Blazor.Minesweeper.Models.Tests;
+
+public class PanelTests
+{
+    [Fact]
+    public void GivenUnrevealedPanel_WhenRevealFunctionCalled_ThenPropertyIsRevealedBecomesTrue()
+    {
+        var panel = new Panel(1,new Coordinate(2,3));
+        panel.IsRevealed = false;
+
+        panel.Reveal();
+
+        panel.IsRevealed.Should().BeTrue();
+    }
+
+    [Fact]
+    public void GivenFlaggedPanel_WhenRevealFunctionCalled_ThenPropertyIsFlaggedBecomesFalse()
+    {
+        var panel = new Panel(1, new Coordinate(2, 3));
+        panel.IsFlagged = true;
+
+        panel.Reveal();
+
+        panel.IsFlagged.Should().BeFalse();
+    }
+
+    [Fact]
+    public void GivenUnrevealedPanel_WhenFlagFunctionCalled_ThenPropertyIsFlaggedIsToggled()
+    {
+        var panel = new Panel(1, new Coordinate(2, 3));
+        panel.IsRevealed = false;
+        panel.IsFlagged = false;
+
+        panel.Flag();
+
+        panel.IsFlagged.Should().BeTrue();
+
+        panel.Flag();
+
+        panel.IsFlagged.Should().BeFalse();
+    }
+
+    [Fact]
+    public void GivenrevealedPanel_WhenFlagFunctionCalled_ThenPropertyIsFlaggedDoesNotChange()
+    {
+        var panel = new Panel(1, new Coordinate(2, 3));
+        panel.IsRevealed = true;
+        panel.IsFlagged = false;
+
+        panel.Flag();
+
+        panel.IsFlagged.Should().BeFalse();
+
+        panel.IsFlagged = true;
+        panel.Flag();
+
+        panel.IsFlagged.Should().BeTrue();
+    }
+}
